6cc1a173091197af1f9a77ff428fedf362a8cdea
naveenreddy018 <naveenreddy310744@gmail.com>
2025-01-11 14:21:55 +0530

    fisrt_change

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Slider_Bar from './components/slide_bar/slide_bar'
+import Response_Bar from './components/reponse_bar/response'
+import StaticExample from './components/reponse_bar/modal'
+
+
+
+function App() {
+  return (
+    <div className='app-container'>
+      <Slider_Bar />
+      <Response_Bar />
+
+     
+    </div>
+  )
+}
+
+export default App
+
